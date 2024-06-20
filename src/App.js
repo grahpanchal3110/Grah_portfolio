cb446c0c8a27206d27aa246d78b347106047f2de
grahpanchal <grahpanchal3110@gmail.com>
2024-06-20 16:19:35 +0530

    Ratio problem solved

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
 import { Toaster } from "react-hot-toast";
 import Contact from "./components/Contact";
-import Header,{HeaderPhone} from "./components/Header";
+import Header, { HeaderPhone } from "./components/Header";
 import Home from "./components/Home";
 import Services from "./components/Services";
 import Testimonial from "./components/Testimonial";
 import Timeline from "./components/Timeline";
 import Work from "./components/Work";
 import Footer from "./components/Footer";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  
-  const [menuOpen,setMenuOpen] = useState(false);
-  const [ratio,setRatio] = useState(window.innerWidth/window.innerHeight);
-  // console.log(ratio);
-  
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [ratio, setRatio] = useState(window.innerWidth / window.innerHeight);
+  console.log(ratio);
+
   useEffect(() => {
-    const resizeRatio = ()=>{
-      setRatio(window.innerWidth/window.innerHeight);
+    const resizeRatio = () => {
+      setRatio(window.innerWidth / window.innerHeight);
     };
-    window.addEventListener("resize",resizeRatio);
-  
+    window.addEventListener("resize", resizeRatio);
+
     return () => {
-      window.removeEventListener("resize",resizeRatio);
+      window.removeEventListener("resize", resizeRatio);
     };
-  }, [ratio])
-  
+  }, [ratio]);
 
-  return ratio < 2 ?(
-   <>
-   <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-   <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-   <Home ratio={ratio}/>
-   <Work/>
-   <Timeline/>
-   <Services/> 
-   <Testimonial/>
-   <Contact/>
-   <Toaster/>
-   <Footer/>
-
-   </>
-  ): (<em id="customMessage">Please Change the ratio to View!</em>);
+  // const isMobile = ratio <= 768;
+  return ratio < 4 ? (
+    <>
+      <HeaderPhone menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Home ratio={ratio} />
+      <Work />
+      <Timeline />
+      <Services />
+      <Testimonial />
+      <Contact />
+      <Toaster />
+      <Footer />
+    </>
+  ) : (
+    <em id="customMessage">Please Change the ratio to View!</em>
+  );
 }
 
 export default App;
